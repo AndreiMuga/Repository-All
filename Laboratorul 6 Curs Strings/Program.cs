b394eaaac9be43c214bdb577ea6b8895cefc33cc
AndreiMuga <andrei.muga@gmail.com>
2024-04-20 12:30:30 +0300

    Mai multe modificari

---
+++ Laboratorul 6 Curs Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Laboratorul_6_Curs_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare strings
+            string myFirstString = "Hello world again";
+            Console.WriteLine(myFirstString);
+
+            int myNumber = 43;
+            Console.WriteLine(myNumber);
+
+            string myNumberString = myNumber.ToString();
+        }
+    }
+}
---
+++ Team 2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+/*
+ Write a method to capitalize first and last letters of a given string. 
+```
+Input: "hello"; 
+Output: "HellO"
+```
+Bonus: Apply the same logic, but capitalize each word of a given string.
+```
+Input: "hello world, how are you?"
+Output: "HellO World, HoW ArE You?"
+```
+ */
+namespace Team_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "hello";
+
+        }
+    }
+}
