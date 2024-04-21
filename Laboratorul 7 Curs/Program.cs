53709b0d605338673471688a695da1fe297fc5e2
AndreiMuga <andrei.muga@gmail.com>
2024-04-21 16:07:44 +0300

    Laboratorul 7 curs

---
+++ Laboratorul 7 Curs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorul_7_Curs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parsing 
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
